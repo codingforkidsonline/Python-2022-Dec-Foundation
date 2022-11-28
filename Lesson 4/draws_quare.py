c2eb1243d8b69a1509bf384852e57a6685266125
Zujiang <zujiang@gmail.com>
2022-11-28 23:44:21 +0800

    add draw square file

---
+++ Lesson 4/draws_quare.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.color("green")
+turtle.pensize(3)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
